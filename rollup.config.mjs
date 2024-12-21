a037ed9aae70b1ddfc576d561bb33fe7e981a60d
Viorel Cojocaru <vio@beanon.com>
2024-12-21 14:09:27 +0100

    build: Update setup
    - use npm
    - run lint and build on the latest

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+
+const INPUT = {
+  'index': './src/index.ts',
+};
+
+const OUTPUT_DIR = 'dist';
+
+export default defineConfig([
+  {
+    input: INPUT,
+    output: {
+      dir: OUTPUT_DIR,
+      format: 'esm',
+      entryFileNames: '[name].mjs',
+      sourcemap: true,
+    },
+    plugins: [typescript({ tsconfig: './tsconfig.json' })],
+  },
+  {
+    input: INPUT,
+    output: {
+      dir: OUTPUT_DIR,
+      format: 'commonjs',
+      entryFileNames: '[name].cjs',
+      sourcemap: true,
+    },
+    plugins: [typescript({ tsconfig: './tsconfig.json' })],
+  },
+]);
